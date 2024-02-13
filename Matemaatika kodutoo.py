7bf701158db3be8b9b975ebe0b2ccfb2f7fd21b2
Eva Tenitski <Eva.Tenitski@tthkee.onmicrosoft.com>
2024-02-13 22:06:27 +0200

    matematika kodutoo

--- Matemaatika kodutoo.py
+++ Matemaatika kodutoo.py
@@ -1,45 +1,58 @@
 import random
 
-print("*** Proverka znanij po matematike ***")
-print()
-print("Vali raskustase:")
-print("1. Prosto")
-print("2. Srendnee")
-print("3. Tazelo")
-difficulty = int(input("Sisesta raskustaseme number (1/2/3): "))
-while difficulty not in [1, 2, 3]:
-    print("viga! Sisesta korrektne raskustaseme number.")
-    difficulty = int(input("Sisesta raskustaseme number (1/2/3): "))
+print("Tere tulemast matemaatika testi!")
 
-total_problems = int(input("Sisesta lahendamiseks vajalike ülesannete arv: "))
-correct_answers = 0
+küsimuste_arv = int(input("Mitu küsimust soovite vastata? "))
+raskustase = int(input("Valige raskustase (1-Kerge, 2-Keskmine, 3-Raske): "))
 
-for _ in range(total_problems):
-    num1 = random.randint(1, 10 ** difficulty)
-    num2 = random.randint(1, 10 ** difficulty)
-    if difficulty == 3:
-        operation = random.choice(['+', '-', '*', '/', '**'])
-    else:
-        operation = random.choice(['+', '-', '*', '/'])
-    problem = f"{num1} {operation} {num2}"
-    correct_answer = eval(problem)
-    user_answer = float(input(f"Lahenda ülesanne: {problem} = "))
-    if user_answer == correct_answer:
-        print("Pravilno!")
-        correct_answers += 1
-    else:
-        print("Nepravilno.")
-
-percentage_correct = (correct_answers / total_problems) * 100
-
-print("\nTulemused:")
-print(f"Said lahendatud {correct_answers} ülesannet {total_problems}-st.")
-print(f"Sinu õigete vastuste protsent: {percentage_correct}%")
-if percentage_correct < 60:
-    print("Hinne 2")
-elif percentage_correct < 75:
-    print("Hinne 3")
-elif percentage_correct < 90:
-    print("Hinne 4")
+if raskustase == 1:
+    num_range = (1, 10)
+elif raskustase == 2:
+    num_range = (10, 100)
+elif raskustase == 3:
+    num_range = (100, 1000)
 else:
-    print("Hinne 5")
+    print("Vigane raskustase. Seadistatakse keskmisele.")
+    num_range = (10, 100)
+
+tehted = ['+', '-', '', '/', '*']
+õiged_vastused = 0
+
+for i in range(küsimuste_arv):
+    tehe = random.choice(tehted)
+    num1 = random.randint(num_range[0], num_range[1])
+    num2 = random.randint(num_range[0], num_range[1])
+
+    if tehe == '+':
+        vastus = num1 + num2
+    elif tehe == '-':
+        vastus = num1 - num2
+    elif tehe == '*':
+        vastus = num1 * num2
+    elif tehe == '/':
+      
+        num2 = random.randint(1, num_range[1])
+        vastus = num1 / num2
+    elif tehe == '**':
+        vastus = num1 ** num2
+
+    print(f"Mis on {num1} {tehe} {num2}?")
+    kasutaja_vastus = float(input("Teie vastus: "))
+
+    if kasutaja_vastus == vastus:
+        print("Õige!")
+        õiged_vastused += 1
+    else:
+        print("Vale!")
+
+tulemus = (õiged_vastused / küsimuste_arv) * 100
+print("\nTest lõpetatud!")
+print("Teie tulemus:", tulemus)
+if tulemus < 60:
+    print("Hinne: 2")
+elif tulemus < 75:
+    print("Hinne: 3")
+elif tulemus < 90:
+    print("Hinne: 4")
+else:
+    print("Hinne: 5")
