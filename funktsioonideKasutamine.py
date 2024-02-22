0bd08c7a90a773824944d9797bd2fc787100f724
Eva Tenitski <Eva.Tenitski@tthkee.onmicrosoft.com>
2024-02-22 15:45:40 +0200

    palgad modul

---
+++ funktsioonideKasutamine.py
@@ -0,0 +1,62 @@
+from MyModul import * 
+from random import *
+##1----------
+#summa_3=Summa(5,8,12)
+#print(summa_3)
+##2---------------
+#a=float(input("Arv1:"))
+#a=float(input("Arv2:"))
+#summa_3=Summa(a,b)
+#print(summa_3)
+##3----------------
+#summa_3=Summa (
+#float (input ("Arv1: ")), 
+#float(input("Arv2: ")),
+#float(input("Arv3: ")))
+#print (summa_3)
+
+
+#(1)
+#a=int(input("Arv1: "))
+#t=input("Tehe: ")
+#b=int(input("Arv2:"))
+#vastus=arithmetic(a,t,b)
+#print(f"{a}{t}{b}=", vastus,sep="")
+
+##(2)
+#for i in range(5):
+#    aasta=randint(1900,2200)
+#    if is_year_leap(aasta):
+#        print(f"Aasta {aasta} on liigaasta")
+#    else:
+#        print(f"Aasta{aasta} on tavaline")from match import*
+
+#(3)
+#a=float(input("Ruuduküilg:"))
+#S,P,D=square(a)
+#print("S",S)
+#print("P",P)
+#print("D",D)
+
+#(4)
+#nr=int(input("Kuu järjekorranumber:"))
+#vastus=season(nr)
+#print(vastus)
+
+#(5)
+#Summa=float (input ("Summa: "))
+#aastad=int(input ("Aastad: "))
+#summa=bank(summa, aastad)
+#print (f"{aastad} aasta ipärast summa on {summa}")
+
+#(6)
+arv=int(input ("Sisesta arv: "))
+if is_prime(arv):
+    print ("Lihtne arv")
+else:
+    print("Liitarv")
+
+
+#7
+
+
